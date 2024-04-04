5ae183451d6e07dc1002875ecd7925b60a9decc6
Starbox <39715563+Starbox7@users.noreply.github.com>
2024-04-05 01:29:02 +0900

    init setting

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,11 +1,18 @@
 import { Provider } from "react-redux";
 import store from "./features/store.js";
 import Router from "./(route)/Router.js";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
     <Provider store={store}>
-      <Router />
+      <QueryClientProvider client={queryClient}>
+        <Router />
+        <ReactQueryDevtools initialIsOpen={true} />
+      </QueryClientProvider>
     </Provider>
   );
 }
